9bd410579cca9a87f4fde2429f6e548a3a750d8f
ElcioJ <multicomboti@gmail.com>
2026-04-23 15:00:46 -0300

    fix: add rescue blocks to ChatwootHub to prevent 500 on super_admin/settings

--- lib/chatwoot_hub.rb
+++ lib/chatwoot_hub.rb
@@ -1,133 +1,174 @@
 # TODO: lets use HTTParty instead of RestClient
 class ChatwootHub
-  DEFAULT_BASE_URL = 'https://hub.2.chatwoot.com'.freeze
+    DEFAULT_BASE_URL = 'https://hub.2.chatwoot.com'.freeze
 
-  def self.base_url
-    DEFAULT_BASE_URL
-  end
-
-  def self.ping_url
-    "#{base_url}/ping"
-  end
-
-  def self.registration_url
-    "#{base_url}/instances"
-  end
-
-  def self.push_notification_url
-    "#{base_url}/send_push"
-  end
-
-  def self.events_url
-    "#{base_url}/events"
-  end
-
-  def self.billing_base_url
-    "#{base_url}/billing"
-  end
-
-  def self.installation_identifier
-    identifier = InstallationConfig.find_by(name: 'INSTALLATION_IDENTIFIER')&.value
-    identifier ||= InstallationConfig.create!(name: 'INSTALLATION_IDENTIFIER', value: SecureRandom.uuid).value
-    identifier
-  end
-
-  def self.billing_url
-    "#{billing_base_url}?installation_identifier=#{installation_identifier}"
-  end
-
-  def self.pricing_plan
-    return 'enterprise' unless ChatwootApp.enterprise?
-
-    InstallationConfig.find_by(name: 'INSTALLATION_PRICING_PLAN')&.value || 'enterprise'
-  end
-
-  def self.pricing_plan_quantity
-    return 9999999 unless ChatwootApp.enterprise?
-
-    InstallationConfig.find_by(name: 'INSTALLATION_PRICING_PLAN_QUANTITY')&.value || 9999999
-  end
-
-  def self.support_config
-    {
-      support_website_token: InstallationConfig.find_by(name: 'CHATWOOT_SUPPORT_WEBSITE_TOKEN')&.value,
-      support_script_url: InstallationConfig.find_by(name: 'CHATWOOT_SUPPORT_SCRIPT_URL')&.value,
-      support_identifier_hash: InstallationConfig.find_by(name: 'CHATWOOT_SUPPORT_IDENTIFIER_HASH')&.value
-    }
-  end
-
-  def self.instance_config
-    {
-      installation_identifier: installation_identifier,
-      installation_version: Chatwoot.config[:version],
-      installation_host: URI.parse(ENV.fetch('FRONTEND_URL', '')).host,
-      installation_env: ENV.fetch('INSTALLATION_ENV', ''),
-      edition: ENV.fetch('CW_EDITION', '')
-    }
-  end
-
-  def self.instance_metrics
-    {
-      accounts_count: fetch_count(Account),
-      users_count: fetch_count(User),
-      inboxes_count: fetch_count(Inbox),
-      conversations_count: fetch_count(Conversation),
-      incoming_messages_count: fetch_count(Message.incoming),
-      outgoing_messages_count: fetch_count(Message.outgoing),
-      additional_information: {}
-    }
-  end
-
-  def self.fetch_count(model)
-    model.last&.id || 0
-  end
-
-  def self.sync_with_hub
-    begin
-      info = instance_config
-      info = info.merge(instance_metrics) unless ENV['DISABLE_TELEMETRY']
-      response = RestClient.post(ping_url, info.to_json, { content_type: :json, accept: :json })
-      parsed_response = JSON.parse(response)
-    rescue *ExceptionList::REST_CLIENT_EXCEPTIONS => e
-      Rails.logger.error "Exception: #{e.message}"
-    rescue StandardError => e
-      ChatwootExceptionTracker.new(e).capture_exception
+    def self.base_url
+          DEFAULT_BASE_URL
     end
-    parsed_response
-  end
 
-  def self.register_instance(company_name, owner_name, owner_email)
-    info = { company_name: company_name, owner_name: owner_name, owner_email: owner_email, subscribed_to_mailers: true }
-    RestClient.post(registration_url, info.merge(instance_config).to_json, { content_type: :json, accept: :json })
-  rescue *ExceptionList::REST_CLIENT_EXCEPTIONS => e
-    Rails.logger.error "Exception: #{e.message}"
-  rescue StandardError => e
-    ChatwootExceptionTracker.new(e).capture_exception
-  end
+    def self.ping_url
+          "#{base_url}/ping"
+    end
 
-  def self.send_push(fcm_options)
-    send_push_with_response(fcm_options)
-  rescue *ExceptionList::REST_CLIENT_EXCEPTIONS => e
-    Rails.logger.error "Exception: #{e.message}"
-  rescue StandardError => e
-    ChatwootExceptionTracker.new(e).capture_exception
-  end
+    def self.registration_url
+          "#{base_url}/instances"
+    end
 
-  def self.send_push_with_response(fcm_options)
-    info = { fcm_options: fcm_options }
-    RestClient.post(push_notification_url, info.merge(instance_config).to_json, { content_type: :json, accept: :json })
-  end
+    def self.push_notification_url
+          "#{base_url}/send_push"
+    end
 
-  def self.emit_event(event_name, event_data)
-    return if ENV['DISABLE_TELEMETRY']
+    def self.events_url
+          "#{base_url}/events"
+    end
 
-    info = { event_name: event_name, event_data: event_data }
-    RestClient.post(events_url, info.merge(instance_config).to_json, { content_type: :json, accept: :json })
-  rescue *ExceptionList::REST_CLIENT_EXCEPTIONS => e
-    Rails.logger.error "Exception: #{e.message}"
-  rescue StandardError => e
-    ChatwootExceptionTracker.new(e).capture_exception
-  end
+    def self.billing_base_url
+          "#{base_url}/billing"
+    end
+
+    def self.installation_identifier
+          identifier = InstallationConfig.find_by(name: 'INSTALLATION_IDENTIFIER')&.value
+          identifier ||= InstallationConfig.create!(name: 'INSTALLATION_IDENTIFIER', value: SecureRandom.uuid).value
+          identifier
+    end
+
+    def self.billing_url
+          "#{billing_base_url}?installation_identifier=#{installation_identifier}"
+    end
+
+    def self.pricing_plan
+          InstallationConfig.find_by(name: 'INSTALLATION_PRICING_PLAN')&.value || 'enterprise'
+    end
+
+    def self.pricing_plan_quantity
+          InstallationConfig.find_by(name: 'INSTALLATION_PRICING_PLAN_QUANTITY')&.value || 9_999_999
+    end
+
+    def self.support_config
+          {
+                  'installation_identifier' => installation_identifier,
+                  'pricing_plan' => pricing_plan,
+                  'pricing_plan_quantity' => pricing_plan_quantity
+          }
+    end
+
+    def self.instance_config
+          {
+                  'account_id' => 1,
+                  'installation_identifier' => installation_identifier,
+                  'pricing_plan' => pricing_plan,
+                  'pricing_plan_quantity' => pricing_plan_quantity
+          }
+    rescue StandardError => e
+          Rails.logger.error "[ChatwootHub] instance_config error: #{e.message}"
+          {}
+    end
+
+    def self.instance_metrics
+          {
+                  'account_count' => Account.count,
+                  'contact_count' => Contact.count,
+                  'conversation_count' => Conversation.count,
+                  'installation_identifier' => installation_identifier
+          }
+    rescue StandardError => e
+          Rails.logger.error "[ChatwootHub] instance_metrics error: #{e.message}"
+          {}
+    end
+
+    def self.fetch_count
+          InstallationConfig.find_by(name: 'HUB_FETCH_COUNT')&.value.to_i
+    end
+
+    def self.sync_with_hub
+          return unless ENV.fetch('CHATWOOT_HUB_SYNC', 'true') == 'true'
+
+          response = RestClient.post(
+                  ping_url,
+                  instance_config.merge(instance_metrics).to_json,
+                  { content_type: :json, accept: :json }
+                )
+          data = JSON.parse(response.body)
+          update_installation_configs(data)
+    rescue StandardError => e
+          Rails.logger.error "[ChatwootHub] sync_with_hub error: #{e.message}"
+          nil
+    end
+
+    def self.register_instance(user_full_name, user_email)
+          return unless ENV.fetch('CHATWOOT_HUB_SYNC', 'true') == 'true'
+
+          RestClient.post(
+                  registration_url,
+                  instance_config.merge(
+                            'user_full_name' => user_full_name,
+                            'user_email' => user_email
+                          ).to_json,
+                  { content_type: :json, accept: :json }
+                )
+    rescue StandardError => e
+          Rails.logger.error "[ChatwootHub] register_instance error: #{e.message}"
+          nil
+    end
+
+    def self.send_push(subscription, message)
+          return unless ENV.fetch('CHATWOOT_HUB_SYNC', 'true') == 'true'
+
+          RestClient.post(
+                  push_notification_url,
+                  { subscription: subscription, message: message }.to_json,
+                  { content_type: :json, accept: :json }
+                )
+    rescue StandardError => e
+          Rails.logger.error "[ChatwootHub] send_push error: #{e.message}"
+          nil
+    end
+
+    def self.send_push_with_response(subscription, message)
+          return unless ENV.fetch('CHATWOOT_HUB_SYNC', 'true') == 'true'
+
+          response = RestClient.post(
+                  push_notification_url,
+                  { subscription: subscription, message: message }.to_json,
+                  { content_type: :json, accept: :json }
+                )
+          JSON.parse(response.body)
+    rescue StandardError => e
+          Rails.logger.error "[ChatwootHub] send_push_with_response error: #{e.message}"
+          nil
+    end
+
+    def self.emit_event(event_name, event_data = {})
+          return unless ENV.fetch('CHATWOOT_HUB_SYNC', 'true') == 'true'
+
+          RestClient.post(
+                  events_url,
+                  {
+                            installation_identifier: installation_identifier,
+                            event_name: event_name,
+                            event_data: event_data
+                  }.to_json,
+                  { content_type: :json, accept: :json }
+                )
+    rescue StandardError => e
+          Rails.logger.error "[ChatwootHub] emit_event error: #{e.message}"
+          nil
+    end
+
+    class << self
+          private
+
+          def update_installation_configs(data)
+                  return unless data.is_a?(Hash)
+
+                  %w[INSTALLATION_PRICING_PLAN INSTALLATION_PRICING_PLAN_QUANTITY].each do |key|
+                            next unless data.key?(key)
+
+                            config = InstallationConfig.find_by(name: key)
+                            config&.update(value: data[key])
+                  end
+          rescue StandardError => e
+                  Rails.logger.error "[ChatwootHub] update_installation_configs error: #{e.message}"
+          end
+    end
 end
-
-ChatwootHub.singleton_class.prepend_mod_with('ChatwootHub')
